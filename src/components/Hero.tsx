1c5c9f11e3ce568d1392ec70c95f76977256943f
Muskan Akhtar <muskanakhter2627@gmail.com>
2024-10-28 06:05:58 +0500

    Second commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,14 +11,14 @@ function Hero() {
         height={600} 
         className="w-full h-full rounded-lg object-cover border-4 md:border-8 border-black border-double"
       />
-      <div className="absolute top-1/4 right-3 md:right-10 flex flex-col items-center justify-center space-y-2">
-        <p className="text-black text-2xl md:text-4xl lg:text-5xl font-bold bg-black bg-opacity-50 px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
+      <div className="absolute top-4 right-3 md:right-10 flex flex-col items-center justify-center">
+        <p className="text-white sm:text-2xl md:text-3xl lg:text-4xl font-bold bg-black bg-opacity-50 px-1 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
           Shadcn UI adoption guide:
         </p>
-        <p className="text-black text-2xl md:text-4xl lg:text-5xl font-bold font-serif bg-black bg-opacity-50 px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
+        <p className="text-white sm:text-2xl md:text-3xl lg:text-4xl font-bold font-serif bg-black bg-opacity-50 px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
           Overview, examples,
         </p>
-        <p className="text-black text-2xl md:text-4xl lg:text-5xl font-bold bg-black bg-opacity-50 px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
+        <p className="text-white sm:text-2xl md:text-3xl lg:text-4xl font-bold bg-black bg-opacity-50 px-2 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
           and alternatives.
         </p>
       </div>
