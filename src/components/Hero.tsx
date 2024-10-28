b2fb9dace8430dc518a0b3d6380b3a556994d892
Muskan Akhtar <muskanakhter2627@gmail.com>
2024-10-28 06:19:10 +0500

    changes in hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,7 +11,7 @@ function Hero() {
         height={600} 
         className="w-full h-full rounded-lg object-cover border-4 md:border-8 border-black border-double"
       />
-      <div className="absolute top-4 right-3 md:right-10 flex flex-col items-center justify-center">
+      <div className="absolute top-1/4 right-3 md:right-10 flex flex-col items-center justify-center">
         <p className="text-white sm:text-2xl md:text-3xl lg:text-4xl font-bold bg-black bg-opacity-50 px-1 py-1 md:px-4 md:py-2 rounded-lg hover:bg-opacity-0">
           Shadcn UI adoption guide:
         </p>
